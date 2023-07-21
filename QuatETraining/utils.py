8135bd33c6283eb47a1cd75ab7aee39e091e071f
Özge Erten <ozge.erten@outlook.com>
2023-07-21 16:40:36 +0200

    dataset prep added to utils

--- QuatETraining/utils.py
+++ QuatETraining/utils.py
@@ -23,26 +23,8 @@ def load_ttl_files(pathfilename):
     return df
 
 
-def convert_to_df_dict(df):
-    hierarchy_dict = {}
-
-    for _, row in df.iterrows():
-        concept = row['s']
-        relation = row['p']
-        other_concept = row['o']
-        if concept in hierarchy_dict:
-        # append the new number to the existing array at this slot
-          hierarchy_dict[concept].append(line[1])
-        else:
-        # create a new array in this slot
-          hierarchy_dict[line[0]] = [line[1]]
-      
-    
-    return hierarchy_dict
-
-
 def has_hierarcy_associative_clash(df):
-    inconsistent_triples = []
+    violating_triples = []
 
     for _, row in df.iterrows():
         concept = row['s']
@@ -52,51 +34,87 @@ def has_hierarcy_associative_clash(df):
         if relation.startswith("skos:"):
             opposite_relation = "skos:related" if relation == "skos:broader" else "skos:broader"
             if df[(df['s'] == concept) & (df['p'] == opposite_relation) & (df['o'] == related_concept)].shape[0] > 0:
-                inconsistent_triples.append((concept, relation, related_concept))
-                inconsistent_triples.append((concept, opposite_relation, related_concept))
+                violating_triples.append((concept, relation, related_concept))
+                violating_triples.append((concept, opposite_relation, related_concept))
             clashes = df[(df['s'] == concept) & (df['p'] == 'skos:related')]['o']
             if related_concept in clashes.values:
-                inconsistent_triples.append((concept, relation, related_concept))
-                inconsistent_triples.append((concept, opposite_relation, related_concept))
+                violating_triples.append((concept, relation, related_concept))
+                violating_triples.append((concept, opposite_relation, related_concept))
 
-    if inconsistent_triples:
-        print("Inconsistent Triples:")
-        count = 0
-        for triple in inconsistent_triples:
-            count = count + 1
-            print(f"{triple[0]} {triple[1]} {triple[2]}")
-            if count == 2:
-              break
+    if violating_triples:
+        updated_kg, negative_df = remove_and_update_kg(df,violating_triples)
+  
     else:
       print("Hiearchy is consistent in terms of hierarcical & associative links clashes")
 
+    return updated_kg, negative_df 
 
-def has_transitive(df):
-    violations = []
 
-    for _, row in df.iterrows():
-        concept = row['s']
-        relation = row['p']
-        related_concept = row['o']
+def remove_and_update_kg(kg, violating_triples):
+  
+    for violating_triple in violating_triples:
+        kg = kg[~((kg['subject'] == violating_triple['subject']) &
+                  (kg['predicate'] == violating_triple['predicate']) &
+                  (kg['object'] == violating_triple['object']))].reset_index(drop=True)
 
-        if relation == "skos:broader":
-            transitive_concepts = df[df['s'] == related_concept]['o']
-            related_concepts = df[df['s'] == concept]['o']
-            
-            i = list(set(transitive_concepts).intersection(related_concepts))
-            for element in i:
-              if (df[df['s'] == element]['p'] == "skos:broader") and ( df[df['s'] == element]['s'] == concept):
-                  violations.append((concept, relation, related_concept))
-                  violations.append((concept, relation, i[0]))
-        
-    if violations:
-      print("Transitivity Violations:")
-      count = 0
-      for violation in violations:
-        count = count + 1
-        #print(f"{violation[0]} {violation[1]} {violation[2]}")
-        if count == 3:
-              break
-    else:
-      print("Hiearachy is consistent in terms of transitivity") 
 
+        negative_df = negative_df.append(violating_triple, ignore_index=True)
+
+    return kg, negative_df
+
+
+
+def split_dataset(df):
+  
+    df_shuffled = df.sample(frac=1, random_state=42)
+    
+    total_samples = df_shuffled.shape[0]
+    train_size = int(0.8 * total_samples)
+    valid_size = int(0.1 * total_samples)
+    test_size = total_samples - train_size - valid_size
+    
+    df_train = df_shuffled.iloc[:train_size]
+    df_valid = df_shuffled.iloc[train_size:train_size + valid_size]
+    df_test = df_shuffled.iloc[train_size + valid_size:]
+    
+    return df_train, df_valid, df_test
+
+
+def convert_to_id_files(df):
+    # Extract unique entities and relations
+    entities = df['entity'].unique()
+    relations = df['relation'].unique()
+
+    # Save entity IDs to a text file
+    with open('entity_ids.txt', 'w') as entity_file:
+        for idx, entity in enumerate(entities):
+            entity_file.write(f"{entity}\t{idx}\n")
+
+    # Save relation IDs to a text file
+    with open('relation_ids.txt', 'w') as relation_file:
+        for idx, relation in enumerate(relations):
+            relation_file.write(f"{relation}\t{idx}\n")
+
+def convert_triples_to_id_files(df,text_df):
+    # Create dictionaries to map entities and relations to their IDs
+    entity_to_id = {}
+    relation_to_id = {}
+
+    # Extract unique entities and relations
+    entities = df['entity'].unique()
+    relations = df['relation'].unique()
+
+    # Assign IDs to entities and relations
+    for idx, entity in enumerate(entities):
+        entity_to_id[entity] = idx
+
+    for idx, relation in enumerate(relations):
+        relation_to_id[relation] = idx
+
+    # Save triple IDs to a text file
+    with open(text_df, 'w') as triples_file:
+        for _, row in df.iterrows():
+            subject_id = entity_to_id[row['entity']]
+            relation_id = relation_to_id[row['relation']]
+            object_id = entity_to_id[row['object']]
+            triples_file.write(f"{subject_id}\t{object_id}\t{relation_id}\n")
